6fc6025d3e10f73b1087ba900f1251c4c79bc497
victus-i7 <109217433+victus-i7@users.noreply.github.com>
2022-08-12 16:39:12 +0500

    Add files via upload

---
+++ lesson2/script.js
@@ -0,0 +1,165 @@
+"use strict"
+
+
+
+// var a=15;
+// let b=10;
+// console.log(++a);
+// console.log(a);
+
+// console.log(b--);
+// b++
+// b++
+// console.log(--b);//decrement
+
+
+
+// let sum = "10";
+
+// sum += "10"
+
+// // sum=sum+50;
+// // sum+=50;
+
+// // sum**=40;
+// // console.log("10" + "7");
+
+
+// // let data = "1241232";
+// // let num = Number(data);
+// // let txt = 3333;
+// // let str = String(txt);
+
+
+
+
+// // console.log(typeof (str));
+// // console.log((str));
+
+// // > , < , >= , <= , == , === , !
+
+
+// // console.log(10 !== "10");
+
+
+
+
+// let sam = 625;
+
+// // console.log(Math.ceil(sam)); // katta tomonga
+
+// // console.log(Math.floor(sam)); // kichik tarafga
+
+// // console.log(Math.round(sam)); // haqiqiy tarafga
+
+// // console.log(Math.pow(sam, 1 / 4)); // darajaga ochiradi
+
+// // console.log(Math.sqrt(sam)); //kv ildiz
+
+// // let namber = Math.random() * 100;
+
+// // console.log(typeof (namber.toFixed(n))*1); // honagacha yahlitlaydi.
+
+// // console.log(Math.round(namber));
+
+// // console.log(Math.max(12, 23, 0.001, 122));
+
+
+
+// let year = 2022;
+
+// let month = 8;
+
+// let day = 12;
+
+// // logical and && ,
+
+// //logical or // mashi tizuvni tekisi ushanda ishlidi
+
+// // if (((year === 2021) || (month === 81) || (day === 12))) {
+
+// //     console.log("2022.08.12");
+// // } else {
+// //     console.log("yil xato");
+// // }
+
+// // Avazbek universitetga o'qishga kirmoqchi. O'qishga kirish uchun IELTS-5.5, math - 40 yoki "25000$" bo'lishi kerak). Agar shartlar bajarilgan bo'lsa o'qishga kirdingiz, bo'lmasa kirolmadingiz deb consoleda ko'rsatsin
+
+// let IELTS = +prompt("Enter your IELTS level");
+
+// let math = +prompt("Enter your Math level");
+
+// let money = +prompt(' Contract =');
+
+// if ((IELTS >= 5.5 && math >= 40) || money >= 25000) {
+//     console.log("Tabriklaymis");
+// } else {
+//     console.log("Yeqildingiz");
+// }
+
+
+
+// console.log(IELTS + math);
+
+
+// let sum1 = prompt("sms");
+
+// let sum2 = 300;
+
+// document.getElementById('text').textContent = sum1 + sum2 + "777777";
+// document.querySelector('.data').innerHTML = `<h1 class="ok">${sum1}</h1>`;
+
+
+// console.log(document.getElementById('text'));
+
+
+
+
+// 1 test
+// let a = 25;
+
+// let b = 15;
+// console.log(b)
+// console.log(a - b);
+
+// test 2
+// let a = 10;
+
+// let b = 5;
+
+// console.log(b);
+// console.log(a);
+
+
+// test 3
+// let a = 10;
+
+// let b = 5;
+
+// let c = (a - b)
+
+// let d = (7 && c || d)
+
+
+// console.log(d);
+
+
+// test 4
+
+// let a = 10;
+
+// let b = 3;
+
+// console.log(a % b);
+
+
+// test 5
+
+// let a = 2;
+
+// let b = 10;
+
+// console.log(a ** b);
+
+
+
